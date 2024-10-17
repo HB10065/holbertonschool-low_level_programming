52a3b224506bff8ed6e6faae324b11610304aa3d
HB10065 <10065@holbertonstudents.com>
2024-10-17 17:47:25 +0000

    gitignore

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * function_name - Prints, single line
+ * @parameterx: Description of parameter x
+(*
+ * Description: Prints "Programing is like building a multilingual puzzle")?
+(* section header: Section description)*
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programing is like building a multilingual puzzle");
+	return (0);
+}
