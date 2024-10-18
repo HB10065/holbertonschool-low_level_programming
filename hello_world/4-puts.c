11bac81ceb70c4458880ea10feb3998bd4ab89dc
HB10065 <10065@holbertonstudents.com>
2024-10-18 12:49:11 +0000

    sheesh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
  * function_name - Prints, single line
- * @parameterx: Description of parameter x
-(*
- * Description: Prints "Programing is like building a multilingual puzzle")?
-(* section header: Section description)*
+ * @parameterx: Something
  * Return: 0
  */
 int main(void)
 {
-	puts("Programing is like building a multilingual puzzle");
+	puts("\"Programing is like building a multilingual puzzle");
 	return (0);
 }
