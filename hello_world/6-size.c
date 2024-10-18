8479b2213ca1ffbdc2f2d6e31d7ef0a64d9f5d89
HB10065 <10065@holbertonstudents.com>
2024-10-18 14:00:07 +0000

    #ifdef_#elif_#endif

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints sizes
+ *Return: 0
+*/
+int main(void)
+{
+#ifdef __x86_64__ /*checks if the architecture is 64 bits */
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+#elif __i386__ /*checks if the architecture is 32 bits */
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+#endif
+	return (0);
+}
