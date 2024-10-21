b1f081a35e1e7220ec6070945e268aca21921f4e
HB10065 <10065@holbertonstudents.com>
2024-10-21 15:37:28 +0000

    printf_if-putchar(while)

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		if (num == 9)
+			putchar(10);
+	}
+	return (0);
+}
