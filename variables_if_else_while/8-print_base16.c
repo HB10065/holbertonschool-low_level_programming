b9da4410799a6a7aa0923c7653671208309ec956
HB10065 <10065@holbertonstudents.com>
2024-10-21 17:22:26 +0000

    pchar_wh_ifel

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints base 16 numbers putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int hexnum = '0', hexlet = 'A';
+
+	while (hexnum <= '9' || hexlet < 'F')
+	{
+		if (hexnum < '9')
+			putchar(hexnum);
+		else
+			putchar(hexlet);
+	}
+	putchar(10);
+	return (0);
+}
