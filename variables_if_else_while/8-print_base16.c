6d3b34a48afa504ab5a4ba2b5a36c28f8f831218
HB10065 <10065@holbertonstudents.com>
2024-10-21 17:32:40 +0000

    miralasCondicionesBien

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,12 +7,18 @@ int main(void)
 {
 	int hexnum = '0', hexlet = 'A';
 
-	while (hexnum <= '9' || hexlet < 'F')
+	while (hexnum <= '9' || hexlet <= 'F')
 	{
-		if (hexnum < '9')
+		if (hexnum <= '9')
+		{
 			putchar(hexnum);
+			hexnum++;
+		}
 		else
+		{
 			putchar(hexlet);
+			hexlet++;
+		}
 	}
 	putchar(10);
 	return (0);
