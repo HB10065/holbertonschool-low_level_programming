2fade22f603a66359d1c41b0d3c8d157c37a7c2f
HB10065 <10065@holbertonstudents.com>
2024-10-21 13:19:29 +0000

    bettypls

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	if (lastdn > 5)
 		printf(" and is greater than 5\n");
 	else if (lastdn == 0)
-	       printf(" and is 0\n");
+		printf(" and is 0\n");
 	else if (lastdn < 6 && lastdn != 0)
 		printf(" and is less than 6 and not 0\n");
 	else
