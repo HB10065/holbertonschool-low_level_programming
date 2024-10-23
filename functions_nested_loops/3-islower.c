9d31e3a3f968edd22b3a645b449570d00ef70e08
HB10065 <10065@holbertonstudents.com>
2024-10-23 13:06:38 +0000

    asdas

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - checks if given letter is lowercase or not
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
