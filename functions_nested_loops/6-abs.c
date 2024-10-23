78c7527d18873e729db61717a8bfb5a128cb64bf
HB10065 <10065@holbertonstudents.com>
2024-10-23 14:05:00 +0000

    valor_absoluto

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - calcuates the absolute value
+ * @n: given number tu calulate
+ * Return: n fi n is positive or zero , -n if n is negative
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
