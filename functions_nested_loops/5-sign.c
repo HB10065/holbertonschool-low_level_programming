2b57fdde7dea62666f149aeddf2f35651ac08447
HB10065 <10065@holbertonstudents.com>
2024-10-23 13:48:29 +0000

    if

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - checks if a number is positive, negative or zero
+ * @n: number to check
+ * Return: 1 if n is positive, 0 if it's zero and -1 if it is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
