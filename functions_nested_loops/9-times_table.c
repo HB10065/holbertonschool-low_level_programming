d34fac03b3d81158b480e628278a4952e62c652f
HB10065 <10065@holbertonstudents.com>
2024-10-24 14:37:11 +0000

    facilito

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Prints multiplication tables from 0 to 9
+ */
+void times_table(void)
+{
+	int c, f, num, ldnum, fdnum;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (f = 0; f < 10; f++)
+		{
+			num = c * f;
+			if (num < 10)
+				_putchar(num + '0');
+			else
+			{
+				ldnum = num % 10;
+				fdnum = (num / 10) % 10;
+				_putchar(fdnum + '0');
+				_putchar(ldnum + '0');
+			}
+			if (f != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (num < 10)
+					_putchar(' ');
+			}
+			else
+				_putchar(10);
+		}
+	}
+}
