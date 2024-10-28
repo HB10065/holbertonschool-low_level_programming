b105aa00392d964e2be85b96915e0b489059bc45
HB10065 <10065@holbertonstudents.com>
2024-10-28 14:12:44 +0000

    num

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int m1, int m2);
+void print_numbers(void);
---
+++ more_functions_nested_loops/print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		_putchar(i + '0');
+	}
+	_putchar(10);
+}
