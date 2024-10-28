50ee076184aa9cfadbaf04e65bf8e13072658a45
HB10065 <10065@holbertonstudents.com>
2024-10-28 14:50:54 +0000

    qdad

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 14)
+	{
+		if (i > 9)
+			_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		i++;
+	}
+	_putchar(10);/*new line*/
+}
