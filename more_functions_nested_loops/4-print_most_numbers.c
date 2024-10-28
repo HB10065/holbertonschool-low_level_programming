70ce8941e47bcce52f0e1114b988e8aac1b9329d
HB10065 <10065@holbertonstudents.com>
2024-10-28 14:26:23 +0000

    a

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+			i++;
+		_putchar(i +'0');
+		i++;
+	}
+	_putchar(10);
+}
