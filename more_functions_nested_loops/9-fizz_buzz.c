4655b93e9748100cbf01c0134eef72809b5191dc
HB10065 <10065@holbertonstudents.com>
2024-10-30 14:57:14 +0000

    1

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = 1;
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0 && i != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if (i % 3 == 0 && i != 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 == 0 && i != 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", i);
