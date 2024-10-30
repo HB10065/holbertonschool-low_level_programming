b40ab4b07d9487121505162c812e98b38b3058d7
HB10065 <10065@holbertonstudents.com>
2024-10-30 15:40:55 +0000

    aver

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ */
+void print_triangle(int size)
+{
+	int i = 1, j, l;
+
+	while (i <= size)
+	{
+		j = size - (1 * i)
+		while (j > 0)
+		{
+			_putchar(' ');
+			j--;
+		}
+		l = 1;
+		while (l <= i)
+		{
+			_putchar('#');
+			l++;
+		}
+		_putchar(10);/*new line*/
+		i++;
+	}
+	if (size <= 0)
+		_putchar(10);
+}
