904445efa6e67bb509f45cf5ddc0e24a4b3b24f8
HB10065 <10065@holbertonstudents.com>
2024-10-30 14:53:11 +0000

    Fizz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - algo
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
