6d25fbaff37286110af8a8b6ca758f0e9f8cf084
HB10065 <10065@holbertonstudents.com>
2024-10-30 12:37:42 +0000

    nose

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0, i <= n, i++)
+	{
+		if (n != 0)
+		{
+			for (j = 0, j <= n, j++)
+				_putchar(' ');
+			_putchar(92); /*inverse diagonal slash*/
+		}
+		_putchar(10); /*new line*/
+	}
+}
