0cf7d2027f099d78eb840ed9493a15b764a2eb59
HB10065 <10065@holbertonstudents.com>
2024-10-30 12:41:19 +0000

    ;

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *
+ * print_diagonal - adadad
+ * @n: se
  */
 void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0, i <= n, i++)
+	for (i = 0; i <= n; i++)
 	{
 		if (n != 0)
 		{
-			for (j = 0, j <= n, j++)
+			for (j = 0; j <= n; j++)
 				_putchar(' ');
 			_putchar(92); /*inverse diagonal slash*/
 		}
