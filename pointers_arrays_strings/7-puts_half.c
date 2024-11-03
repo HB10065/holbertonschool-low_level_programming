9cdb89d808a55639d1ca38a5229e38acde5c0be9
HB10065 <10065@holbertonstudents.com>
2024-11-03 23:35:59 +0000

    a

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - algo
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != 0)
+		i++;
+	if (i % 2 != 0)
+		i--;
+	j = i / 2;
+	while (j <= i && str[j] != 0)
+	{
+		_putchar(str[j]);
+	}
+	_putchar(10);/*new line*/
+}
