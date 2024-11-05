8a6487a6cdbc16cf702929d441c41c930cb866b3
HB10065 <10065@holbertonstudents.com>
2024-11-05 14:33:34 +0000

    a

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memset - replaces the value of n characters on a string *s
+ * @s: given string
+ * @b: character given
+ * @n: number of bytes to be changed
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (*s);
+}
