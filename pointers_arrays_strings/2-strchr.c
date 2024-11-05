4d863080d753b89034d697e7a67ba89c30b4d4a7
HB10065 <10065@holbertonstudents.com>
2024-11-05 15:41:36 +0000

    encuentra-el-caracter

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - encuentra un caracter en un string
+ * @s: String dado
+ * @c: caracter a encontrar
+ * Return: Null si no esta el caracter, si existe, un pointer hacia Ãl
+ */
+char *_strchr(char *s, char c)
+{
+	int size, i;
+
+	size = _strlen(*s);
+	for (i = 0 ; s[i] != c || s[i] != '\0'; i++)
+		{
+		}
+	if (i == size)
+		return ('\0');
+	else
+		return (s[i]);
+}
