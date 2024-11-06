523eab13df34de386aa2e3b9ca91d4803b1e658c
HB10065 <10065@holbertonstudents.com>
2024-11-06 13:46:31 +0000

    Betty_tiene_la_culpa

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,12 +15,12 @@ char *_strchr(char *s, char c)
 	{
 		size++;
 	}
-	for (i = 0 ; i <= size; i++)
+	for (i = 0 ; i < size; i++)
 		{
 			if (s[i] == c)
 				break;
 		}
-	if (i > size)
+	if (i + 1 > size)
 		return (NULL);
 	else
 		return (&s[i]);
