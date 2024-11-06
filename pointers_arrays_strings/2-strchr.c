21ee5d2e29392bc11cea1dbbaf35aa547f2826c1
HB10065 <10065@holbertonstudents.com>
2024-11-06 13:43:21 +0000

    nul

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,12 @@ char *_strchr(char *s, char c)
 	{
 		size++;
 	}
-	for (i = 0 ; s[i] != c && s[i] != '\0'; i++)
+	for (i = 0 ; i <= size; i++)
 		{
+			if (s[i] == c)
+				break;
 		}
-	if (i == size)
+	if (i > size)
 		return (NULL);
 	else
 		return (&s[i]);
