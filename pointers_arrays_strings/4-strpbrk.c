778fde7cb17fb0236d0fcd09abde9d9dc6024821
HB10065 <10065@holbertonstudents.com>
2024-11-06 15:44:06 +0000

    aver

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strpbrk - Returns the memory direction of the first byte of s that is also on accept
+ * @s: String to analize
+ * @accept: String with accepted characters
+ * Return: memory direction of a byte of s or NULL if there is no match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
