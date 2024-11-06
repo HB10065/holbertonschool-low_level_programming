5ebabd82fa9250501332576188511d24db2ea035
HB10065 <10065@holbertonstudents.com>
2024-11-06 15:04:32 +0000

    j+k

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,12 +8,11 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	unsigned int k = 0;
+	unsigned int i, j, k = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j + k] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
