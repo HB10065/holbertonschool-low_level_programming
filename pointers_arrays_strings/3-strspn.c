d518deb65fcf3f0833a42a231f420555baa68801
HB10065 <10065@holbertonstudents.com>
2024-11-06 15:03:16 +0000

    3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - Counts how many bytes of accept are in s also
+ * @s: String
+ * @accept: subString
+ * Return: k, amount of equal bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+			}
+		}
+	}
+	return (k);
+}
