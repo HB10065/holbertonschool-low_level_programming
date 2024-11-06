2210eaf6d4a0cf5afa13124e9e492cd20415b093
HB10065 <10065@holbertonstudents.com>
2024-11-06 15:23:55 +0000

    l

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, k = 0;
+	unsigned int i, j, k = 0, l;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		l = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
+				l = 1;
 				k++;
 				break;
 			}
+		}
+		if (l == 0)
+		{
+			break;
 		}
 	}
 	return (k);
