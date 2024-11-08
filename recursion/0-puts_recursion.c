56772f36b591b4a7a7ace529952b0111ecb71c6b
HB10065 <10065@holbertonstudents.com>
2024-11-08 19:42:30 +0000

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/*
+ * _puts_recursion - imitates put
+ * @s: String to print
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar(10); /* new line */
+}
