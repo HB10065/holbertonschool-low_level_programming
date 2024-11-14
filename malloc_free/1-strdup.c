60e3873619a9eec6f17e2e2d047b64ac2f0f5e40
HB10065 <10065@holbertonstudents.com>
2024-11-14 17:11:05 +0000

    a

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *a;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (str[i] != '\0')
+		{
+			i++;
+		}
+
+		a = malloc(i);
+
+		if (a == NULL)
+		{
+			return (NULL);
+		}
+
+		for (; j <= i; j++)
+		{
+			a[j] = str[j];
+		}
+		return (a);
+	}
+
+}
