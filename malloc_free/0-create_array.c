8e6a236e24c0c2536b83df334445fe240ec0e14c
HB10065 <10065@holbertonstudents.com>
2024-11-14 15:37:02 +0000

    aloha

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: tamaÃo del array
+ * @c: primer caracter
+ * Return: direccion de c
+ */
+char *create_array(unsigned int size, char c)
+{
+	int *a;
+	*a = malloc(size);
+	a[0] = c
+	return (a);
+}
+
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+En esta carpeta estan ejercicios para aprender sobre malloc y free
