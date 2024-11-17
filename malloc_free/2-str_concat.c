177ab42fff826e6f1085acaa5f2fb4963eb6ccc0
HB10065 <10065@holbertonstudent.com>
2024-11-17 23:42:11 +0000

    a

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,17 +14,34 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	a = malloc(i + j - 2);
 
-	for (x = 0; x < i; x++)
+	if (*s1 != NULL && *s2 != NULL)
 	{
-		a[x] = s1[x];
-	}
+		a = malloc(i + j - 2);
+		for (x = 0; x < i; x++)
+			a[x] = s1[x];
 
-	for (y = 0; y < j; y++)
-	{
-		a[x] = s2[y];
-		x++;
+		for (y = 0; y < j; y++)
+		{
+			a[x] = s2[y];
+			x++;
+		}
+		return (a);
 	}
-	return (a);
+	else if (*s1 == NULL && *s2 != NULL)
+	{
+		a = malloc(j - 1);
+		for(x = 0; x < j; x++)
+			a[x] = s2[x];
+		return (a);
+	}
+	else if (*s1 != NULL && *s2 == NULL)
+	{
+		a = malloc(i - 1);
+		for (x = 0; x < i; x++)
+			a[x] = s1[x];
+		return (a);
+	}
+	else
+		return (NULL);
 }
