209f5035818ef508967e50a78b3c31681f0fb5f1
HB10065 <10065@holbertonstudents.com>
2024-12-05 18:30:14 +0000

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- *
+ * print_list - prints the contents of list_t
+ * @h: the list
+ * Return: amount of Nodes of the linked list
  */
 size_t print_list(const list_t *h)
 {
