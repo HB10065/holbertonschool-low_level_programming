5b961e8407b63aca4ebe562a1ec3ac1db4afc38f
HB10065 <10065@holbertonstudents.com>
2024-12-05 17:51:17 +0000

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,11 +2,10 @@
 #define MAIN_H
 #include <unistd.h>
 #include <stdio.h>
-typedef struct list_s
-{
+typedef struct list_t{
 	char *str;
 	unsigned int len;
-	struct list_s *next;
+	struct list_t *next;
 }
-size_t print_list(const list_s *h);
+size_t print_list(const list_t *h);
 #endif
