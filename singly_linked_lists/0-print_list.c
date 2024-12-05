26b690a9f975cc2c519682f3d568754367194822
HB10065 <10065@holbertonstudents.com>
2024-12-05 18:25:11 +0000

    str NULL

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,7 +9,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		nodos++;
 	}
