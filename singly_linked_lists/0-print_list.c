470e084555615579d4652c3c7ed7ff8e2f719ebc
HB10065 <10065@holbertonstudents.com>
2024-12-05 18:08:03 +0000

    const

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@ size_t print_list(const list_t *h)
 {
 	int nodos;
 	const list_t a = *h;
-	while (a -> next != NULL)
+	while (a->next != NULL)
 	{
 		printf("[%d] %s\n", a->len, a->str);
 		a = a->*next;
