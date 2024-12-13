1faf2ae507f82f052cadb8db5f0a2ed6511d0058
HB10065 <10065@holbertonstudents.com>
2024-12-13 01:00:43 +0000

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+/**
+ * precursion - noc
+ * @a: lugar del string
+ * @s: string
+ */
+void precursion(int *a, char *s)
+{
+        if (s[*a + 1] != '\0')
+        {
+                *a = *a + 1;
+                precursion(a, s);
+		_putchar(s[*a]);
+        }
+        else
+        {
+                _putchar(10);
+        }
+}
+/**
+ * _print_rev_recursion - prints a string backwards
+ * @s: String to print
+ */
+void _print_rev_recursion(char *s)
+{
+        int a = 0;
+
+        precursion(&a, s);
+}
