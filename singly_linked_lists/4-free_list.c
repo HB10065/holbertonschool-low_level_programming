fce01428c827ea5b10c9e8a28a261472ae9c71e9
HB10065 <10065@holbertonstudents.com>
2025-01-10 22:42:23 +0000

    4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *past;
+
+	current = head;
+	while (current->next != NULL)
+	{
+		past = current;
+		current = current->next;
+		free(past->str);
+	}
+	free(current->str);
+}
