f7e40b8442a860c6489c5357fd179f5bcb066fad
HB10065 <10065@holbertonstudents.com>
2025-01-10 20:56:46 +0000

    1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,9 @@
+#include "lists.h"
+
 /**
- *
+ * list_len - cuenta la cantidad de nodos de una lista
+ * @h: primer nodo
+ * Return: cantidad de nodos en la lista
  */
 size_t list_len(const list_t *h)
 {
