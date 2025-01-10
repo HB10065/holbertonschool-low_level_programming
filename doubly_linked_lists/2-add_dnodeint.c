aaf6e82b8dd7ec23a46a27300df02c9004856b94
HB10065 <10065@holbertonstudents.com>
2025-01-10 23:43:17 +0000

    2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint *current;
+
+	if (head == NULL)
+		return (NULL);
+
+	current->n = n;
+	current->next = *head;
+	current->prev = NULL;
+
+	return (current);
+}
