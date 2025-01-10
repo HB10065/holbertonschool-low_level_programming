8c833c7fad80126269a925e2f945d53d553c8b87
HB10065 <10065@holbertonstudents.com>
2025-01-10 21:15:36 +0000

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node - agrega un nodo al inicio de una single list
+ * @head: siguiente nodo
+ * @str: dato a guardar
+ * Return: direccion al nuev nodo, si falla retorna NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *h;
+	char *str_copy;
+
+	if (str == NULL)
+		return (NULL);
+
+	str_copy = strdup(str);
+	if (str_copy == NULL)
+		return (NULL);
+	h->str = str_copy;
+	h->next = head;
+	return (h);
+}
