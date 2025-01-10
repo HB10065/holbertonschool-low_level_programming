472c0f91a477c57007c9b2f551c3c0f08869501f
HB10065 <10065@holbertonstudents.com>
2025-01-10 21:53:24 +0000

    2

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - agrega un nodo al inicio de una single list
+ * @head: siguiente nodo
+ * @str: dato a guardar
+ * Return: direccion al nuev nodo, si falla retorna NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+        list_t *h;
+        char *str_copy;
+        unsigned int i = 0;
+
+        if (str == NULL)
+                return (NULL);
+
+        str_copy = strdup(str);
+        if (str_copy == NULL)
+        {
+                free(str_copy);
+                return (NULL);
+        }
+        while (str_copy[i] != '\0')
+                i++;
+        h = malloc(sizeof(list_t));
+        if (h == NULL)
+                return (NULL);
+        h->str = str_copy;
+        h->len = i;
+        h->next = NULL;
+	*head->next = h
+        return (h);
+}
