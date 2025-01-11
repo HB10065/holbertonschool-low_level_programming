97eb07d12f08c1327dfe329b6646b97bf8a96853
HB10065 <10065@holbertonstudents.com>
2025-01-11 01:33:52 +0000

    6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - suma los valores guardados de una lista
+ * @head: primer nodo de la lista
+ * Return: suma de los valores
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	int i = 0;
+
+	if (head == NULL)
+		return (0);
+
+	if (head->next == NULL)
+		return (head->n);
+
+	current = head->next;
+	while (current->next != NULL)
+	{
+		i += current->n;
+		current = current->next;
+	}
+	i += current->n;
+	return (i);
+}
