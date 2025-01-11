85dcb38a94f411432871edc6e1614d1a2ed560a1
HB10065 <10065@holbertonstudents.com>
2025-01-11 00:04:09 +0000

    head change

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,12 +8,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *current;
 
 	current = malloc(sizeof(dlistint_t));
+	if (current == NULL)
+		return (NULL);
+
 	current->n = n;
 	if (head != NULL)
 		current->next = *head;
 	else
+	{
 		current->next = NULL;
+		*head = current;
+	}
 	current->prev = NULL;
 
-	return (*current);
+	return (current);
 }
