e96ff320d4ee915a76249bbb16a2a94a89b16cf6
HB10065 <10065@holbertonstudents.com>
2025-01-11 00:45:24 +0000

    3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - agrega un nodo al final de una lista doble
+ * @head: inicio de la lista
+ * @n: dato a guardar en el nodo
+ * Return: direccion al nuevo nodo
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		(*head) = new_node;
+	}
+	else if (*head->next == NULL)
+	{
+		new_node->prev = (*head);
+		(*head)->next = new_node;
+	}
+	else
+	{
+		current = (*head)->next;
+		while (current->next != NULL)
+			current = current->next;
+		
+		new_node->prev = current;
+		current->next = new_node;
+	}
+	return (new_node);
+
+}
