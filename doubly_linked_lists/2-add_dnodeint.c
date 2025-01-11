552976f37b743b18cc64bf8bc2535dffccb2b531
HB10065 <10065@holbertonstudents.com>
2025-01-11 00:21:29 +0000

    redo

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,29 @@
 #include "lists.h"
 
 /**
- *
+ * add_dnodeint - agrega un nodo al inicio de una lista doble
+ * @head: inicio de la lista
+ * @n: dato a guardar
+ * Return: direccion del nuevo nodo
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *current;
+	dlistint_t *new_node;
 
-	current = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 	if (current == NULL)
 		return (NULL);
 
-	current->n = n;
-	if (head != NULL)
-	{
-		current->next = *head;
-		(*head)->prev = current;
-	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (*head == NULL)
+		new_node->next = NULL;
 	else
-		current->next = NULL;
-	current->prev = NULL;
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node
+	}
+	*head = new_node;
 
-	*head = current;
-	return (current);
+	return (new_node);
 }
