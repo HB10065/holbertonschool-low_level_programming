c47fbf930d068bf5cb08f6ab44a70e3d9e81fe22
HB10065 <10065@holbertonstudents.com>
2025-01-11 01:17:54 +0000

    encontrar nodo

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - encuentra un nodo en una lista y devuelve su ubicacion
+ * @head: primer nodo de la lista
+ * @index: posicion en la lista del nodo a encontrar
+ * Return: Ubicacion del nodo, retorna NULL si el nodo no existe
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i = ;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (index == 0)
+		return (head);
+
+	if (head->next == NULL)
+		return (NULL);
+
+	current = head->next;
+	while (current->next != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+	if (i != index)
+		return (NULL);
+	else
+		return (current);
+
+}
